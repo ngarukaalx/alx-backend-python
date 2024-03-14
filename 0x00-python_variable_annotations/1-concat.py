a9d679c3c0214f3894d3463fb8c4e06189246f7f
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-14 12:46:28 +0000

    annotated function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains func concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns concatenated string
+      args: str1 & str2 - str
+    """
+    return str1 + str2
