598786e904724a521074c92a78f3507769458c88
ngarukaalx <hiramkabuagi@gmail.com>
2024-03-18 19:27:14 +0000

    callback

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """multiple coroutines at the same time"""
     listDelays = []
+    waits = []
     for _ in range(n):
-        calls = await wait_random(max_delay)
-        bisect.insort(listDelays, calls)
+        task = asyncio.create_task(wait_random(max_delay))
+        task.add_done_callback(lambda i: listDelays.append(i.result()))
+        waits.append(task)
+    for wait in waits:
+        await wait
     return listDelays
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,5 +7,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Task:
+    """craeting tasks for async"""
     task = asyncio.create_task(wait_random(max_delay))
     return task
